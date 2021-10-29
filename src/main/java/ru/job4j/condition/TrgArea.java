20a4a0e418bfd172564ce04972f6a715196f907a
andmmm <andmmm@mail.ru>
2021-10-29 11:17:10 +0400

    9. Площадь треугольника. [#175474 #221930] fix-1

--- src/main/java/ru/job4j/condition/TrgArea.java
+++ src/main/java/ru/job4j/condition/TrgArea.java
@@ -2,39 +2,9 @@ package ru.job4j.condition;
 
 public class TrgArea {
 
-    /**
-     * Метод должен вычислить площадь треугольника.
-     *
-     * @return Вернуть площадь, если треугольник существует или -1, если
-     * треугольника нет.
-     */
     public static double area(double a, double b, double c) {
-        double rsl = -1;
-        double p = TrgArea.semiPerimeter(a, b, c);
-        if (TrgArea.existTriangle(a, b, c)) {
-            rsl = Math.sqrt(p * (p - a) * (p - b) * (p - c));
-        }
-        return rsl;
-    }
-
-    /**
-     * Метод вычисления полупериметра по длинам сторон.
-     */
-    private static double semiPerimeter(double a, double b, double c) {
-        return (a + b + c) * 0.5;
-    }
-
-    /**
-     * Метод проверяет можно ли построить треугольник с такими длинами сторон.
-     */
-    private static boolean existTriangle(double a, double b, double c) {
-        boolean exist = false;
-        if (a > 0 && b > 0 && c > 0) {
-            if ((a + c > b) && (a + b > c) && (c + b > a)) {
-                exist = true;
-            }
-        }
-        return exist;
+        double p = (a + b + c) * 0.5;
+        return Math.sqrt(p * (p - a) * (p - b) * (p - c));
     }
 
     public static void main(String[] args) {
