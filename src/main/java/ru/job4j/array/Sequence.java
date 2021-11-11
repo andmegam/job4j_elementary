e3ff52b92f81ceeaf12778a4542d1a980aa2c19c
andmmm <andmmm@mail.ru>
2021-11-11 23:45:03 +0400

    6.6.2. Двухмерный массив. Циклы. [#306725]

--- src/main/java/ru/job4j/array/Sequence.java
+++ src/main/java/ru/job4j/array/Sequence.java
@@ -16,7 +16,7 @@ public class Sequence {
         array[2][2] = 9;
 
         for (int i = 0; i < array.length; i++) {
-            for (int j = 0; j < array.length; j++) {
+            for (int j = 0; j < array[i].length; j++) {
                 System.out.print(array[i][j]);
             }
             System.out.println();
